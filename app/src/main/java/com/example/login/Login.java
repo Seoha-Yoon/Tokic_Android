68e05e05ceca6de293603b76861b672cb22f3fdc
MinyeongHonggit config --global user.email agka00161@gmail.comcd C:/Users/user/Desktop/Git <78523467+MinyeongHong@users.noreply.github.com>
2021-08-17 12:46:42 +0900

    17 August commit remove firebase auth

--- app/src/main/java/com/example/login/Login.java
+++ app/src/main/java/com/example/login/Login.java
@@ -7,31 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-
-import org.jetbrains.annotations.NotNull;
 
 public class Login extends AppCompatActivity {
-
-    private FirebaseAuth mFirebaseAuth; //파이어베이스 인증
-    private Toast toast;
-    private static final String TAG = "AnonymousAuth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        //mDatabaseRef = FirebaseDatabase.getInstance().getReference();
-
         Button btn_start=findViewById(R.id.btn_start);
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,34 +23,7 @@ public class Login extends AppCompatActivity {
             }
         });
 
-
         }
-
-    @Override
-    protected void onStart() {
-        super.onStart();         // Check if user is signed in (non-null) and update UI accordingly
-        FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    private void updateUI(FirebaseUser user) {
-        if (user==null){
-            mFirebaseAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
-                        Toast.makeText(Login.this, "Hello! Wellcome Tokic",Toast.LENGTH_SHORT);
-                        FirebaseUser user=mFirebaseAuth.getCurrentUser();
-                        updateUI(user);
-                    }
-                    else {
-                        updateUI(null);
-                    }
-                }
-            });
-        }
-
-    }
 
 }
 
